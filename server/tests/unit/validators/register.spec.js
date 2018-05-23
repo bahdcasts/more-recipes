e9be33cbc1dda40a0c905941f39dff9f068e27c6
Kati Frantz <frantzenterprises777@gmail.com>
2018-05-23 06:29:16 +0100

    the validateName function tests

--- server/tests/unit/validators/register.spec.js
+++ server/tests/unit/validators/register.spec.js
@@ -5,15 +5,32 @@ const { RegisterUserValidator } = validators
 describe('The RegisterUserValidator class', () => {
   describe('The validateName function ', () => {
     test('The validateName function adds a required error to the errors array if name is not provided', () => {
+      // ARRANGE
       const validator = new RegisterUserValidator({
         email: 'bahdcoder@gmail.com'
       })
 
+      // ACTION
       validator.validateName()
 
-      const errors = validator.errors
+      // ASSERTION
+      const { errors } = validator
       expect(errors).toEqual([
         'The name is required.'
+      ])
+    })
+    test('adds an error if name is less than 5 characters', () => {
+      // ARRANGE
+      const validator = new RegisterUserValidator({
+        name: 'bahd'
+      })
+      // ACTION
+
+      validator.validateName()
+
+      // ASSERTION
+      expect(validator.errors).toEqual([
+        'The name must be longer than 5 characters.'
       ])
     })
   })
